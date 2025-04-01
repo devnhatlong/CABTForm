854bfd4273805789144bb082b134edd1c52d9cc0
Raito Nguyen <raito.n@itcgroup.io>
2025-04-01 17:34:09 +0700

    fix UI dashboard

--- client/src/pages/Dashboard/views/Dashboard.js
+++ client/src/pages/Dashboard/views/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Routes, Route, useNavigate } from 'react-router-dom';
-import { UserOutlined, GroupOutlined, ProfileOutlined, FormOutlined } from '@ant-design/icons';
+import { UserOutlined, GroupOutlined, ProfileOutlined, FormOutlined, IdcardOutlined, CarOutlined, FireOutlined } from '@ant-design/icons';
 import { Menu, Layout } from 'antd';
 import { useSelector } from 'react-redux';
 
@@ -8,20 +8,29 @@ import { NavbarLoginComponent } from "../../../components/NavbarLoginComponent/N
 import { getItem } from "../../../utils/utils";
 import { AdminUser } from "../../AdminUser/views/AdminUser";
 import { AdminDepartment } from "../../AdminDepartment/views/AdminDepartment";
+import '../styles/style.css';
 
 const { Sider, Content } = Layout;
 
 export const Dashboard = () => {
     const user = useSelector((state) => state.user);
     const [collapsed, setCollapsed] = useState(true);
+    const [openKeys, setOpenKeys] = useState([]); // State to manage open keys
 
     const navigate = useNavigate();
 
     const menuItemStyle = {
         whiteSpace: 'normal',
         lineHeight: 'normal',
-        fontSize: "14px",
-        fontWeight: "700"
+        fontSize: "15px",
+        fontWeight: "600",
+    };
+
+    const menuChildrenItemStyle = {
+        display: "flex",
+        alignItems: "center",
+        fontSize: "15px",
+        fontWeight: "600",
     };
     
     const items = [
@@ -31,18 +40,42 @@ export const Dashboard = () => {
             icon: <UserOutlined />,
             style: menuItemStyle,
             children: [
-                getItem('Người dùng', 'user', <UserOutlined />, null, null, menuItemStyle),
-                getItem('Đơn vị / Phòng ban', 'department', <GroupOutlined />, null, null, menuItemStyle),
+                getItem('Người dùng', 'user', null, null, menuChildrenItemStyle),
+                getItem('Đơn vị / Phòng ban', 'department', null, null, menuChildrenItemStyle),
             ]
         },
 
-        user?.role === "admin" && {
-            key: 'form',
-            label: 'Form',
-            icon: <ProfileOutlined />,
+        {
+            key: 'social_order',
+            label: 'Vụ việc về TTXH',
+            icon: <IdcardOutlined />,
             style: menuItemStyle,
             children: [
-                getItem('Danh sách form', 'forms', <FormOutlined />, null, null, menuItemStyle),
+                getItem('Danh sách vụ việc', 'social_order_list', null, null, menuChildrenItemStyle),
+                getItem('Tra cứu đối tượng', 'criminal_information_lookup', null, null, menuChildrenItemStyle),
+                getItem('Thống kê số liệu', 'statistics_social_order_incident', null, null, menuChildrenItemStyle),
+            ]
+        },
+
+        {
+            key: 'traffic',
+            label: 'Giao thông',
+            icon: <CarOutlined />,
+            style: menuItemStyle,
+            children: [
+                getItem('Danh sách TNGT', 'traffic_incident_list', null, null, menuChildrenItemStyle),
+                getItem('Thống kê vụ TNGT', 'statistics_traffic_incident', null, null, menuChildrenItemStyle),
+            ]
+        },
+
+        {
+            key: 'fire_protection_and_prevention',
+            label: 'Phòng cháy chữa cháy',
+            icon: <FireOutlined />,
+            style: menuItemStyle,
+            children: [
+                getItem('Danh sách vụ cháy/nổ', 'fires_and_explosions_list', null, null, menuChildrenItemStyle),
+                getItem('Thống kê vụ cháy/nổ', 'statistics_fires_and_explosions', null, null, menuChildrenItemStyle),
             ]
         },
     ].filter(Boolean);  // Remove null items
@@ -51,6 +84,12 @@ export const Dashboard = () => {
         if (key === 'user') navigate('/admin/user');
         else if (key === 'department') navigate('/admin/department');
         else if (key === 'forms') navigate('/forms');
+    };
+
+    const onOpenChange = (keys) => {
+        // Only keep the last opened key
+        const latestOpenKey = keys.find(key => !openKeys.includes(key));
+        setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
     };
 
     const toggleCollapsed = () => {
@@ -85,18 +124,34 @@ export const Dashboard = () => {
                     collapsed={collapsed}
                     onCollapse={toggleCollapsed}
                     width={250}
-                    style={{ background: '#fff', boxShadow: '2px 0 8px 0 rgba(29, 35, 41, 0.05)' }}
+                    style={{
+                        background: '#fff',
+                        boxShadow: '2px 0 8px 0 rgba(29, 35, 41, 0.05)',
+                        height: '100vh', // Chiều cao toàn màn hình
+                        position: 'fixed', // Đứng yên so với toàn trang
+                        left: 0, // Đặt ở bên trái
+                        overflowY: 'auto', // Hiển thị thanh cuộn dọc nếu nội dung dài
+                    }}
                 >
                     <Menu
                         mode="inline"
                         style={{ borderRight: 0 }}
                         items={items}
                         onClick={handleOnClick}
+                        openKeys={openKeys} // Controlled open keys
+                        onOpenChange={onOpenChange} // Handle open/close
                         defaultSelectedKeys={['1']}
-                        defaultOpenKeys={collapsed ? [] : ['admin']}
                     />
                 </Sider>
-                <Content style={{ margin: '0px 12px', padding: 18, background: '#fff', minHeight: '280px' }}>
+                <Content
+                    style={{
+                        marginLeft: collapsed ? 80 : 250, // Đẩy nội dung sang phải tùy thuộc vào trạng thái của Sider
+                        margin: '0px 12px',
+                        padding: 18,
+                        background: '#fff',
+                        minHeight: '280px',
+                    }}
+                >
                     <Routes>
                         <Route path="/admin/user" element={<AdminUser />} />
                         <Route path="/admin/department" element={<AdminDepartment />} />
