4abb7a9f20f751c2c2efed9d605a83f583d27d89
Raito Nguyen <raito.n@itcgroup.io>
2025-04-02 15:28:18 +0700

    remove old code

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,15 +1,11 @@
 const userRouter = require("./userRouter");
 const departmentRouter = require("./departmentRouter");
-const formRoutes = require("./formRoutes");
-const responseRoutes = require("./responseRoutes");
 const fieldOfWorkRoutes = require("./fieldOfWorkRouter");
 const { notFound, errHandler } = require("../middlewares/errorHandler");
 
 const initRoutes = (app) => { 
     app.use("/api/user", userRouter);
     app.use("/api/department", departmentRouter);
-    app.use("/api/forms", formRoutes);
-    app.use("/api/responses", responseRoutes);
     app.use("/api/field-of-work", fieldOfWorkRoutes);
 
     app.use(notFound);
--- server/routes/responseRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const ctrls = require("../controllers/responseController");
-const { verifyAccessToken } = require("../middlewares/verifyToken");
-
-router.post("/", verifyAccessToken, ctrls.createResponse);
-router.get("/:formId", ctrls.getResponsesByForm);
-router.get("/detail/:id", ctrls.getResponseById);
-
-module.exports = router;
