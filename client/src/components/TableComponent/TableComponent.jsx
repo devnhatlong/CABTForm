77c18852ee1d047c98fa96b5c0d7cf01924d0273
Raito Nguyen <raito.n@itcgroup.io>
2025-04-03 16:47:59 +0700

    update

--- client/src/components/TableComponent/TableComponent.jsx
+++ client/src/components/TableComponent/TableComponent.jsx
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from 'react'
-import { Table } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Table, Modal } from 'antd'; // Import Modal
 import Loading from '../LoadingComponent/Loading';
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
 import { StyledTable } from './style';
 
 const TableComponent = (props) => {
   const { selectionType = 'checkbox', data = [], isLoading = false, columns = [], handleDeleteMultiple, resetSelection } = props;
   const [selectedRowKeys, setSelectedRowKeys] = useState([]);
   const user = useSelector((state) => state?.user);
+
   // rowSelection object indicates the need for row selection
   const rowSelection = {
     onChange: (selectedRowKeys, selectedRows) => {
@@ -20,29 +21,46 @@ const TableComponent = (props) => {
   }, [resetSelection]);
 
   const handleDeleteAll = () => {
-    handleDeleteMultiple(selectedRowKeys);
-  }
+    Modal.confirm({
+      title: 'Xác nhận xóa',
+      content: 'Bạn có chắc chắn muốn xóa tất cả các mục đã chọn không?',
+      okText: 'Xóa',
+      cancelText: 'Hủy',
+      onOk: () => {
+        handleDeleteMultiple(selectedRowKeys); // Thực hiện xóa nếu người dùng xác nhận
+      },
+    });
+  };
 
   return (
     <Loading isLoading={isLoading}>
-        {user?.role === "admin" && selectedRowKeys.length > 0 && (
-          <div style={{ backgroundColor: '#1677ff', color: '#fff', fontWeight: 'bold', padding: '10px', cursor: 'pointer'}} onClick={handleDeleteAll}>
-            Xóa tất cả
-          </div>
-        )}
-        <StyledTable
-        style={{ fontSize: '16px' }}
-          rowSelection={{
-          type: selectionType,
-            ...rowSelection,
+      {user?.role === 'admin' && selectedRowKeys.length > 0 && (
+        <div
+          style={{
+            backgroundColor: '#1677ff',
+            color: '#fff',
+            fontWeight: 'bold',
+            padding: '10px',
+            cursor: 'pointer',
           }}
-          columns={columns}
-          dataSource={data}
-          {...props}
-          bordered
-        />
+          onClick={handleDeleteAll}
+        >
+          Xóa tất cả
+        </div>
+      )}
+      <StyledTable
+        style={{ fontSize: '16px' }}
+        rowSelection={{
+          type: selectionType,
+          ...rowSelection,
+        }}
+        columns={columns}
+        dataSource={data}
+        {...props}
+        bordered
+      />
     </Loading>
-  )
-}
+  );
+};
 
-export default TableComponent
+export default TableComponent;
