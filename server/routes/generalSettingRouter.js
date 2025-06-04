2269120dc4c01fc7677ad3a3d3be24bcce771ad2
Raito Nguyen <dev.nhatlong@gmail.com>
2025-06-04 22:24:04 +0700

    fix permission for get generalSetting

--- server/routes/generalSettingRouter.js
+++ server/routes/generalSettingRouter.js
@@ -3,8 +3,8 @@ const ctrls = require("../controllers/generalSettingController");
 const { verifyAccessToken, isAdmin } = require("../middlewares/verifyToken");
 
 router.post("/", [verifyAccessToken, isAdmin], ctrls.createGeneralSetting);
-router.get("/", [verifyAccessToken, isAdmin], ctrls.getGeneralSettings);
-router.get("/:id", [verifyAccessToken, isAdmin], ctrls.getGeneralSettingById);
+router.get("/", [verifyAccessToken], ctrls.getGeneralSettings);
+router.get("/:id", [verifyAccessToken], ctrls.getGeneralSettingById);
 router.put("/", [verifyAccessToken, isAdmin], ctrls.updateGeneralSetting); // Sử dụng key thay vì id
 
 module.exports = router;
